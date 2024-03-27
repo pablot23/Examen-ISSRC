837542341876a1c7dd03d9c80fb4017f7c3e0484
pablot23 <plablot@fibertel.com.ar>
2024-03-27 20:40:14 -0300

    Update ejer1.js

--- ejer1.js
+++ ejer1.js
@@ -1,22 +1,22 @@
-function figurageometrica(numLados) {
-    if (numLados < 0) {
+function figurageometrica(lados) {
+    if (lados < 0) {
       console.log("Error");
       return;
     }
   
-    if (numLados == 0) {
+    if (lados == 0) {
       console.log("Círculo");
-    } else if (numLados == 4) { 
+    } else if (lados == 4) { 
       console.log("cuadrado");
-    } else if (numLados == 3) { 
+    } else if (lados == 3) { 
       console.log("ciculo");
-    } else if (numLados == 5) {
+    } else if (lados == 5) {
       console.log("Pentágono");
-    } else if (numLados == 6) {
+    } else if (lados == 6) {
       console.log("Hexágono");
     } else {
       console.log("Polígono");
     }
   }
   
-  figurageometrica(4); // Esto mostrará "Cuadrado" en la consola
+  figurageometrica(4); 
